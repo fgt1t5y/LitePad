2d678961ffbd1edf4bfa83facf32a579616b4bdd
望 <1501034130@qq.com>
2024-07-30 21:28:45 +0800

    使用lightningcss处理样式文件

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,20 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { PrimeVueResolver } from "@primevue/auto-import-resolver";
+import { browserslistToTargets } from "lightningcss";
+import browserslist from "browserslist";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    cssMinify: "lightningcss",
+  },
+  css: {
+    transformer: "lightningcss",
+    lightningcss: {
+      targets: browserslistToTargets(browserslist(">= 0.25%")),
+    },
+  },
   plugins: [vue(), Components({ resolvers: [PrimeVueResolver()] })],
   resolve: {
     alias: {
