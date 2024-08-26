a69e937505a03a2b4f7eb19c41a99b3c78120f2d
望 <1501034130@qq.com>
2024-08-26 22:47:12 +0800

    改进构建脚本

--- build.js
+++ build.js
@@ -1,22 +1,30 @@
 import nwbuild from "nw-builder";
+import { copyFile } from "fs";
 
-nwbuild({
-  mode: "build",
-  srcDir: "./dist",
-  glob: false,
-  flavor: 'normal',
-  managedManifest: {
-    name: "litepad",
-    main: "index.html",
-    window: {
-      id: "litepad-gui",
-      title: "LitePad",
-      position: "center",
-      height: 660,
-      width: 1060,
-      min_height: 600,
-      min_width: 800,
-      icon: "./appicon.png",
+const build = () => {
+  nwbuild({
+    mode: "build",
+    srcDir: "./dist",
+    glob: false,
+    flavor: "normal",
+    managedManifest: {
+      name: "litepad",
+      main: "index.html",
+      window: {
+        id: "litepad-gui",
+        title: "LitePad",
+        position: "center",
+        height: 660,
+        width: 1060,
+        min_height: 600,
+        min_width: 800,
+        icon: "./appicon.png",
+      },
     },
-  },
+  });
+};
+
+copyFile("package.json", "dist/package.json", (err) => {
+  if (err) throw err;
+  build();
 });
