9b20ef48c92153b99e2f5f8d2e8f4d4bcbb4ab01
望 <1501034130@qq.com>
2024-09-28 20:58:54 +0800

    笔记保存后同步tree和tabs状态

--- src/views/NoteView.vue
+++ src/views/NoteView.vue
@@ -17,16 +17,20 @@
 <script setup lang="ts">
 import Editor from "@/components/editor/Editor.vue";
 import { db } from "@/db";
+import { usePageTabs } from "@/utils/usePageTabs";
+import { useShared } from "@/utils/useShared";
 import { useToast } from "primevue/usetoast";
 import { ref, onBeforeUnmount, watchEffect } from "vue";
 import { useRoute } from "vue-router";
 
 const editorRef = ref<InstanceType<typeof Editor>>();
 const noteTitle = ref<string>();
-const noteContent = ref<string>();
+const noteContent = ref<string>("");
 
 const route = useRoute();
 const toast = useToast();
+const s = useShared();
+const tabs = usePageTabs();
 const noteId = Number(route.params.id);
 
 db.notes.get(noteId).then((note) => {
@@ -41,6 +45,12 @@ const saveNote = async () => {
     name: noteTitle.value,
     content: noteContent.value,
   });
+
+  s.updateNote(noteId, {
+    name: noteTitle.value,
+  });
+
+  tabs.setLabel(noteId, noteTitle.value!);
 
   toast.add({ severity: "success", summary: "保存成功", life: 1000 });
 };
